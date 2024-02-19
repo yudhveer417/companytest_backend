d1fe706a415af163ce33644e5927d38583227382
yudhveer417 <160490121+yudhveer417@users.noreply.github.com>
2024-02-19 17:36:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+// backend/index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Task = require('./models/Task');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/taskApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(cors());
+
+// Routes
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get('/tasks/:id', getTask, (req, res) => {
+  res.json(res.task);
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.put('/tasks/:id', getTask, async (req, res) => {
+  if (req.body.title != null) {
+    res.task.title = req.body.title;
+  }
+  if (req.body.description != null) {
+    res.task.description = req.body.description;
+  }
+  
+  try {
+    const updatedTask = await res.task.save();
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    await Task.findOneAndDelete({_id:req.params.id});
+    res.json({ message: 'Task deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getTask(req, res, next) {
+  let task;
+  try {
+    task = await Task.findById(req.params.id);
+    if (task == null) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.task = task;
+  next();
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/Task.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+ 
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
